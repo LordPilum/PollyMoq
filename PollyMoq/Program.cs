380cf7e3806c4047c5387cc4910f2031b98d2977
Jan Kjetil Myklebust <jan.kjetil.myklebust@enoro.com>
2019-05-22 14:53:14 +0200

    Setting up code for the application.

--- PollyMoq/Program.cs
+++ PollyMoq/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace PollyMoq
+﻿using Autofac;
+
+namespace PollyMoq
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<PollyModule>();
+            builder.RegisterType<Application>().As<IStartable>().SingleInstance();
+
+            builder.Build();
         }
     }
 }
