f2853b47ac377e1497810418bf762891b214075d
Jan Kjetil Myklebust <jan.kjetil.myklebust@enoro.com>
2019-05-24 14:50:52 +0200

    Setting up Moq.

---
+++ PollyMoq/Moq/RetryAndFallbackMoq.UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using Polly;
+using Polly.Registry;
+
+namespace PollyMoq.Moq
+{
+	[TestFixture]
+    public class RetryAndFallbackMoq
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var policyRegistry = new PolicyRegistry {{PolicyRegistryKeys.Default, Policy.NoOpAsync()}};
+
+            var app = new Mock<Application>(policyRegistry);
+            app.Object.Start();
+        }
+    }
+}
